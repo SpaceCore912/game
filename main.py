8cd2ed1459f70c36069a66456f195b641060606a
Tykhon Byshkin <tykhonbyshkin07@gmail.com>
2024-10-12 22:18:08 -0400

    add some more logic and some text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import necessary modules
 import random
-
+import time
+start_time=time.time()
 # Define the ranks and suits
 ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
 suits = ("hearts", "diamonds", "clubs", "spades")
@@ -10,21 +11,20 @@ deck = []
 for rank in ranks:
             for suit in suits:
                 deck.append((rank,suit))
-#print(deck)
+print(deck)
 
 
-# Shuffle the deck 
-for x in range(len(deck)):
-    deck[x]=deck[random.randint(1,len(deck))-1]
+random.shuffle(deck)
+print(deck)
 
-lendeck=len(deck)
-print(lendeck)
+time.sleep(3)
 
 #Split the deck into two hands
 deck1=deck[:26]
-deck1extra=[]
 deck2=deck[26:]
+deck1extra=[]
 deck2extra=[]
+
 print(deck1)
 
 def card_comparison(p1_card, p2_card):
@@ -41,33 +41,122 @@ def card_comparison(p1_card, p2_card):
         return 2
     else:
         return 0
-def play_round(player1_hand, player2_hand):
+def play_round():
     """Play a single round of the game.
 		That is, each player flips a card, and the winner is determined using the card_comparison function
 		if both players flip the same value card, call the war function
 	"""
-    p1card=deck.pop[0]
-    p2card=deck.pop[0]
-    output=card_comparison(p1card,p2card)
-    if output=0:
-        war()
-    elif output=1:
-        deck1.extend([p1card,p2card])
-    elif output=2:
-        dock2.extend([p1card,p2card])
-def war(player1_hand, player2_hand):
+    
+    i=0
+    while deck1 != [] and deck2  != []:
+        p1card=deck1[0]
+        p2card=deck2[0]
+        
+        output=card_comparison(p1card,p2card)
+        
+        if output==0:
+            
+            
+            
+            list=[]
+            war(list,deck1,deck2)
+        
+
+        if output==1:
+            deck1extra.extend([p1card,p2card])
+            deck1.pop(0)    
+            deck2.pop(0)
+        if output==2:
+            deck2extra.extend([p1card,p2card])
+            deck1.pop(0)    
+            deck2.pop(0)
+        print(len(deck1))
+        print(len(deck2))
+        
+        
+        i+=1
+        print(str(i)+"times")
+    print("hello")
+    time.sleep(0.3)
+    return deck1extra, deck2extra
+
+def war(list,deck1,deck2):
     """Handle the 'war' scenario when cards are equal.
 		recall the rules of war, both players put 3 cards face down, 
 		then both players flip face up a 4th card. The player with the stronger
 		card takes all the cards.		
 	"""
-    deck
-    while 
-
-def play_game():
-    """Main function to run the game."""
-    # Your code here
- 
-# Call the main function to start the game
-play_game()
+    global deck1extra, deck2extra
+    list1=list
+    i=0
+    print("peace")
     
+    while i<5:
+        list1.append(deck1[0])
+        list1.append(deck2[0])
+        deck1.pop(0)
+        deck2.pop(0)
+
+        if deck1==[]:
+            if deck1extra==[]:
+                 print('the end')
+                 return False
+            deck1=deck1extra
+            random.shuffle(deck1)
+        
+        if deck2==[]:
+            if deck2extra==[]:
+                 print('the end')
+                 return False
+            deck2=deck2extra
+            random.shuffle(deck2)
+        i+=1
+    output=card_comparison(list1[-6],list1[-1]) 
+    if output==0:
+        war(list1,deck1,deck2)
+    if output==1:
+        deck1extra.extend(list1)
+        
+    if output==2:
+        deck2extra.extend(list1)
+        
+    
+    
+    return False
+
+    
+#main game
+i=0
+
+while (deck1 != [] and deck2 != []):
+        
+        play_round()
+        print("\nround "+ str(i+1))
+        print("\n\n")
+        print("deck1: "+ str(deck1))
+        print("\ncards taken from player 2: " + str(deck1extra))
+        print("\n\n")
+        print("deck2: "+ str(deck2))
+        print("\ncards taken from player 1: " + str(deck2extra))
+        print("\n\n")
+        if  len(deck1)==0:
+            deck1=deck1extra
+            random.shuffle(deck1)
+            deck1extra=[]
+        if   len(deck2)==0:
+            deck2=deck2extra
+            random.shuffle(deck2) 
+            deck2extra=[]   
+        print("deck1 shuffled: "+ str(deck1))
+        print("\ndeck2 shuffled: "+ str(deck2))
+        time.sleep(0.3)
+        i+=1
+if deck1==[]:
+     print("\n\nplayer1 lost")
+if deck2==[]:
+     print("\n\nplayer2 lost")
+
+
+end_time=time.time()
+print(str(i+1) +" rounds were played")
+print(end_time-start_time)
